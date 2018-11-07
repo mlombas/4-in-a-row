b4ac3170a81d9eec7f41ec29ca7f7917507e871e
mocoma <mocomaminecraft@gmail.com>
2018-11-07 14:15:38 +0100

    Fixed indentation and added TODO comments

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import pygame
 
+#TODO move this to maybe grid class?
 BG_COLOR = (0, 0, 0)
 TEAM1_COLOR = (255, 0, 0)
 TEAM2_COLOR = (255, 255, 0)
 
 class Grid(object):
-        def __init__(self, n_rows, n_cols, color=(0, 0, 255)):
-            self.n_rows = n_rows
-            self.n_cols = n_cols
-            self.color = color
+    def __init__(self, n_rows, n_cols, color=(0, 0, 255)):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.color = color
 
-            self._fichas = []
+        self._fichas = []
 
-        def add_ficha(self, ficha):
-            if not isinstance(ficha, Ficha): raise TypeError("Ur a dumbass")
+    def add_ficha(self, ficha):
+        if not isinstance(ficha, Ficha): raise TypeError("Ur a dumbass")
             
-            self._fichas.append(ficha)
+        self._fichas.append(ficha)
 
-        def draw(self, surface):
-            def draw_cell(rect, offset):
-                pygame.draw.rect(surface, self.color, rect)
-                ellipse_rect = pygame.Rect(rect.x + rect.width * offset, rect.y + rect.height * offset, rect.width * (1 - 2*offset), rect.height * (1 - 2*offset))
-                pygame.draw.ellipse(surface, BG_COLOR, ellipse_rect)
+    def draw(self, surface):
+        #TODO delet this                                                                (jokes on you)
+        def draw_cell(rect, offset):
+            pygame.draw.rect(surface, self.color, rect)
+            ellipse_rect = pygame.Rect(rect.x + rect.width * offset, rect.y + rect.height * offset, rect.width * (1 - 2*offset), rect.height * (1 - 2*offset))
+            pygame.draw.ellipse(surface, BG_COLOR, ellipse_rect)
 
-            #calculate shit
-            offset_percent = 0.05
-            surface_w, surface_h = surface.get_size()
-            grid_rect = pygame.Rect(surface_w * offset_percent, surface_h * offset_percent, surface_w * (1 - 2*offset_percent),  surface_h * (1 - 2*offset_percent))
-            cell_w, cell_h = int(grid_rect.width / self.n_cols), int(grid_rect.height / self.n_rows)
+        #TODO draw fichas
+            
+        #calculate shit
+        offset_percent = 0.05
+        surface_w, surface_h = surface.get_size()
+        grid_rect = pygame.Rect(surface_w * offset_percent, surface_h * offset_percent, surface_w * (1 - 2*offset_percent),  surface_h * (1 - 2*offset_percent))
+        cell_w, cell_h = int(grid_rect.width / self.n_cols), int(grid_rect.height / self.n_rows)
 
-            #draw shit
-            for x in range(grid_rect.left, grid_rect.right - cell_w, cell_w):
-                for y in range(grid_rect.top, grid_rect.bottom - cell_h, cell_h):
-                    draw_cell(pygame.Rect(x, y, cell_w, cell_h), 0.05)
+        #draw shit
+        for x in range(grid_rect.left, grid_rect.right - cell_w, cell_w):
+            for y in range(grid_rect.top, grid_rect.bottom - cell_h, cell_h):
+                draw_cell(pygame.Rect(x, y, cell_w, cell_h), 0.05)
 
-        def map_coords(self, x, y):
-            pass
-
+    def map_coords(self, x, y):
+        pass
+    
+    #TODO gravity, highlight, update, moving methods...
 
 class Ficha(object):
     def __init__(self, x, y, radius, color):
