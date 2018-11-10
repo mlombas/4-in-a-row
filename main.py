53598c5d0d52be5a0d18b6f26fea5144276c1fab
mocomaminecraft <mocomaminecraft@gmail.com>
2018-11-10 20:37:04 +0100

    now it works using surfaces and not drawings

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+#Author: mocoma
+
+#for you to remember dumbass
+# 1 = team red (1)
+# -1 = team blue (2)
+# 0 = no team
+
 import pygame
+
+#TODO add errors
 
 class Grid(object):
     gravity = pygame.math.Vector2(0, 1)
     
-    surfaces = {} #TODO add surfaces instead of fucking colors
-    
+    surfaces = {}     
+
     def __init__(self, n_cols, n_rows, width=800, height=800, color=(0, 0, 255)):
         self.n_rows = n_rows
         self.n_cols = n_cols
 
         self.grid_rect = pygame.Rect(0, 0, width, height)
-        self.cell_w = width / n_rows
-        self.cell_h = width / n_cols
+        self.cell_w = int(width / n_rows)
+        self.cell_h = int(width / n_cols)
 
         self._fichas = []
 
+        self.surfaces["cell"] = pygame.transform.scale(pygame.image.load("cell.png"), (self.cell_w, self.cell_h))
+        self.surfaces["team1"] = pygame.transform.scale(pygame.image.load("red_ficha.png"), (self.cell_w, self.cell_h))
+        self.surfaces["team-1"] = pygame.transform.scale(pygame.image.load("blue_ficha.png"), (self.cell_w, self.cell_h))
+
     def add_ficha_ip(self, col, row, team):
-        ficha = Ficha(col * self.cell_w, row * self.cell_h, min(self.cell_w, self.cell_h), self.surfaces[f"team{team}"])
+        ficha = Ficha(pygame.Rect(col * self.cell_w, row * self.cell_h, self.cell_w, self.cell_h), self.surfaces[f"team{team}"])
         self.add_ficha(ficha)
 
     def add_ficha(self, ficha): 
@@ -34,25 +47,33 @@ class Grid(object):
         #draw shit
         for x in range(self.n_cols):
             for y in range(self.n_rows):
-                surface.blit(self.surfaces["cell"], (x * cell_w, y * cell_h)
+                surface.blit(self.surfaces["cell"], (x * cell_w, y * cell_h))
     
-    #TODO highlight, update, moving methods...
+    #TODO update
+    #TODO anyone_moving
+    #TODO get_grid_status
+    #TODO highlight_row, highlight_col
+    #TODO is_ended 
 
 class Ficha(object):
     def __init__(self, rect, surface):
         self.rect = rect
-        self.surface = surface
+        self.surface = pygame.transform.scale(surface, rect.size)
 
     def draw(self, surface):
-        surface.blit(self.surface, rect)
-
+        surface.blit(self.surface, self.rect)
+ 
+    #TODO update
+    
     def __repr__(self):
         return f"Ficha({self.rect}, {self.color})"
 
 
 main_surface = pygame.display.set_mode((800, 800))
 g = Grid(7, 7)
-g.draw(main_surface)
 g.add_ficha_ip(3, 2, 1)
+g.add_ficha_ip(0, 0, -1)
+g.add_ficha_ip(5, 3, -1)
+g.draw(main_surface)
 pygame.display.update()
 while True: pass
