8516b60990714fcabb5cc442ee730fe473ae469f
mocomaminecraft <mocomaminecraft@gmail.com>
2018-11-13 19:39:21 +0100

    almost works but I need to fix the winning checking fucntion

--- main.py
+++ main.py
@@ -6,11 +6,14 @@
 # 0 = no team
 
 import pygame
+import sys
+import time
 
-#TODO add errors
+class GridError(Exception):
+    pass
 
 class Grid(object):
-    gravity = pygame.math.Vector2(0, 1)
+    gravity = pygame.math.Vector2(0, 980)
     
     surfaces = {}     
 
@@ -29,7 +32,13 @@ class Grid(object):
         self.surfaces["team-1"] = pygame.transform.scale(pygame.image.load("blue_ficha.png"), (self.cell_w, self.cell_h))
 
     def add_ficha_ip(self, col, row, team):
-        ficha = Ficha(pygame.Rect(col * self.cell_w, row * self.cell_h, self.cell_w, self.cell_h), self.surfaces[f"team{team}"])
+        if self.anyone_moving():
+            raise GridError("cant add ficha when anyone is moving")
+        
+        if self.get_grid_status()[col][row] != 0:
+            raise GridError("cant add ficha in top of another")
+
+        ficha = Ficha(pygame.Rect(col * self.cell_w, row * self.cell_h, self.cell_w, self.cell_h), self.surfaces[f"team{team}"], team)
         self.add_ficha(ficha)
 
     def add_ficha(self, ficha): 
@@ -37,43 +46,150 @@ class Grid(object):
             
         self._fichas.append(ficha)
 
+    def get_fichas(self):
+        return tuple(self._fichas)
+
     def draw(self, surface):
+        pygame.draw.rect(surface, (0,0,0), surface.get_rect())
+
         for ficha in self._fichas:
             ficha.draw(surface)
-
-        #calculate shit
-        cell_w, cell_h = int(self.grid_rect.width / self.n_cols), int(self.grid_rect.height / self.n_rows)
 
         #draw shit
         for x in range(self.n_cols):
             for y in range(self.n_rows):
-                surface.blit(self.surfaces["cell"], (x * cell_w, y * cell_h))
+                surface.blit(self.surfaces["cell"], (x * self.cell_w, y * self.cell_h))
     
-    #TODO update
-    #TODO anyone_moving
-    #TODO get_grid_status
-    #TODO highlight_row, highlight_col
-    #TODO is_ended 
+    def update(self, dt):
+        for ficha in self._fichas: 
+            ficha.update(dt, self)
+    
+    def anyone_moving(self):
+        return any(f.is_moving() for f in self._fichas)
+
+    def get_grid_status(self):
+        if self.anyone_moving():
+           raise GridError("cant get status if anyone moving dumbass")
+
+        representation = [[0 for y in range(self.n_rows)] for x in range(self.n_cols)]
+        for ficha in self._fichas:
+            x, y = ficha.rect.center
+            col = int(x / self.cell_w)
+            row = int(y / self.cell_h)
+
+            representation[col][row] = ficha.team
+        
+        return representation
+    
+    def is_ended(self):
+        status = self.get_grid_status()
+ 
+        for x in range(self.n_cols):
+            curr_count = 0
+            last_team = 0
+            for y in range(self.n_rows):
+                if status[x][y] != last_team:
+                    last_team = status[x][y]
+                    curr_count = 1
+                else: curr_count += 1
+
+                if curr_count == 4 and last_team != 0: return last_team
+
+        for y in range(self.n_rows):
+            curr_count = 0
+            last_team = 0
+            for x in range(self.n_cols):
+                if status[x][y] != last_team:
+                    last_team = status[x][y]
+                    curr_count = 1
+                else: curr_count += 1
+            
+                if curr_count == 4 and last_team != 0: return last_team
+        
+        #TODO fix this
+        for ry in range(-self.n_rows, 1):
+            for x in range(self.n_cols):
+                y = ry + x
+                if y > 0:
+                    if status[x][y] != last_team:
+                        last_team = status[x][ry]
+                        curr_count = 1
+                    else: curr_count += 1
+            
+                    if curr_count == 4 and last_team != 0: return last_team
+
+        for ry in range(-self.n_rows, 1):
+            for x in range(self.n_cols - 1, -1, -1):
+                y = ry + x
+                if y > 0:
+                    if status[x][y] != last_team:
+                        last_team = status[x][ry]
+                        curr_count = 1
+                    else: curr_count += 1
+            
+                    if curr_count == 4 and last_team != 0: return last_team
+
+        return 0
 
 class Ficha(object):
-    def __init__(self, rect, surface):
+    def __init__(self, rect, surface, team):
         self.rect = rect
+        self.vel = pygame.math.Vector2(0, 0)
         self.surface = pygame.transform.scale(surface, rect.size)
+        self.team = team
 
     def draw(self, surface):
         surface.blit(self.surface, self.rect)
  
-    #TODO update
-    
+    def update(self, dt, grid):
+        self.vel += grid.gravity * dt
+        offset = self.vel * dt
+        next_move = self.rect.move(offset.x, offset.y)
+
+        collides_with_ficha = any(next_move.colliderect(other.rect) for other in grid.get_fichas() if other != self.rect)
+        is_in_grid = bool(grid.grid_rect.contains(next_move))
+        if collides_with_ficha or not is_in_grid:
+            self.vel = pygame.math.Vector2(0, 0)
+        else:
+            self.rect = next_move
+
+    def is_moving(self):
+        return self.vel.length() > 1e2
+
     def __repr__(self):
         return f"Ficha({self.rect}, {self.color})"
 
 
 main_surface = pygame.display.set_mode((800, 800))
 g = Grid(7, 7)
-g.add_ficha_ip(3, 2, 1)
-g.add_ficha_ip(0, 0, -1)
-g.add_ficha_ip(5, 3, -1)
-g.draw(main_surface)
-pygame.display.update()
+
+last_time = time.clock()
+while True:
+    dt = time.clock() - last_time
+    last_time = time.clock()
+
+    for evt in pygame.event.get():
+        if evt.type == pygame.MOUSEBUTTONUP and not g.anyone_moving():
+            x, y = evt.pos
+            col = int(x / g.cell_w)
+            row = int(y / g.cell_h)
+            
+            try:
+                g.add_ficha_ip(col, row, 1 if evt.button == 1 else -1)
+            except GridError:
+                pass
+    
+        if evt.type == pygame.QUIT:
+            sys.exit()
+
+    if not g.anyone_moving():
+        v = g.is_ended()
+        if v != 0:
+            print(v)
+            sys.exit()
+        
+    g.update(dt)
+    g.draw(main_surface)
+    pygame.display.update()
+
 while True: pass
